e762138c39baf7603872d513be9c88c1b86815aa
Adhikarikaushal <kaushaladhikari006@gmail.com>
2026-08-24 20:53:51 +0545

    created the class User by using the abstract class

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        ADMIN = 'ADMIN', 'Admin/CEO'
+        MANAGER = 'MANAGER', 'Manager'
+        STAFF = 'STAFF', 'General Staff'
+
+    role = models.CharField(
+        max_length=20,
+        choices=Role.choices,
+        default=Role.STAFF
+    )
+    phone=models.CharField(max_length=20, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.username} (self{self.role})"
