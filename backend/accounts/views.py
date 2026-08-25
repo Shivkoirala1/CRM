d92cff5c11534b239786461eb9ae978b1f68644f
Adhikarikaushal <kaushaladhikari006@gmail.com>
2026-08-25 12:45:07 +0545

    api for get request

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from crm.utils import api_response
+from .serializers import UserSerializer
 
-# Create your views here.
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def me(request):
+    serializer = UserSerializer(request.user)
+    return api_response(
+        success=True,
+        message="User retrieved successfully.",
+        data=serializer.data,
+        status_code=200
+    )
+
