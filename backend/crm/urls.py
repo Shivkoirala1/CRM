70a7dbfe3a95d67679606eb929192fd8f0a19839
Adhikarikaushal <kaushaladhikari006@gmail.com>
2026-08-27 11:54:16 +0545

    added the urls of the clients

--- backend/crm/urls.py
+++ backend/crm/urls.py
@@ -23,14 +23,16 @@ from rest_framework_simplejwt.views import (
 )
 from accounts.views import me
 from leads.views import LeadViewSet
+from clients.views import ClientViewSet
 
 router = DefaultRouter()
 router.register('leads', LeadViewSet, basename='lead')
-
+router.register('clients', ClientViewSet, basename='client')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/me/', me, name='me'),
+    path('api/', include(router.urls)),
     path('api/', include(router.urls)),
 ]
