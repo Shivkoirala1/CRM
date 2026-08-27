bd3c11e49f77735a1395986e4d8be863a7fa76c7
Adhikarikaushal <kaushaladhikari006@gmail.com>
2026-08-27 11:52:28 +0545

    created the ClientViewSet

--- backend/clients/views.py
+++ backend/clients/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from crm.utils import api_response
+from .models import Client
+from .serializers import ClientSerializer
 
-# Create your views here.
+
+class ClientViewSet(viewsets.ModelViewSet):
+    queryset = Client.objects.filter(is_archived=False).order_by('-created_at')
+    serializer_class = ClientSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+        return api_response(
+            success=True,
+            message="Clients retrieved successfully.",
+            data=serializer.data
+        )
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return api_response(
+            success=True,
+            message="Client retrieved successfully.",
+            data=serializer.data
+        )
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return api_response(
+            success=True,
+            message="Client created successfully.",
+            data=serializer.data,
+            status_code=201
+        )
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return api_response(
+            success=True,
+            message="Client updated successfully.",
+            data=serializer.data
+        )
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.is_archived = True
+        instance.save()
+        return api_response(
+            success=True,
+            message="Client archived successfully.",
+            data=None
+        )
